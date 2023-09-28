8db70ba35c1c578672a5ed392da6a658be4763a8
MarioEduardojr <134741824+MarioEduardojr@users.noreply.github.com>
2023-09-28 11:34:40 -0300

    Update olimpiadas e copa - questao 1.c

--- olimpiadas e copa - questao 1.c
+++ olimpiadas e copa - questao 1.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
 
-    int ano;
+    int X;
 
-    scanf("%d", &ano);
+    scanf("%d", &X);
 
-    if(ano % 2 != 0){
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", ano);
-    }else if(ano < 1896){
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", ano);
-    }else if((ano - 1930) % 4 == 0){
-        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.", ano);
-    }else if((ano - 1896) % 4 == 0){
-        printf("Os jogos Olimpicos de Verao ocorreram no ano de %d.", ano);
-    }else if(ano < 1896)
+    if(X > 2022){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", X);
+    }else if(X % 2 != 0){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", X);
+    }else if(X < 1896){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", X);
+    }else if(X != 1916 && X != 1940 && X != 1944 && ((X - 1896) %4 == 0)){
+        printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", X);
+    }else if(X >= 1930 && X != 1942 && X != 1946 && ((X - 1930) % 4 == 0)){
+        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", X);
+    }else if(X < 1930 && ((X - 1930) % 4 == 0)){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", X);
+    }else if(X == 1916 || X == 1940 || X == 1942 || X == 1944 || X == 1946){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", X);
+    }
 
     return 0;
 }
